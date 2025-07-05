4586bf1b9930b554d9260ccd744b797f53e1320e
VISHAL <palvishal111@gmail.com>
2025-07-05 11:03:04 +0530

    Updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,6 @@ const Header = () => {
       <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>Welcome to our app</h2>
       <p className='mb-8 max-w-mb'>Let's start with a quick product tour</p>
       <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all' >Get Started</button>
-      <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all' >Login</button> 
     </div>
   )
 }
